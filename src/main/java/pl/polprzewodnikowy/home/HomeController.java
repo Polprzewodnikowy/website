62a8028b58a8510fd0face35321ebd389138a470
Polprzewodnikowy <polprzewodnikowy@gmail.com>
2018-07-27 17:52:51 +0200

    Created basic layout, added test user entity

--- src/main/java/pl/polprzewodnikowy/home/HomeController.java
+++ src/main/java/pl/polprzewodnikowy/home/HomeController.java
@@ -3,6 +3,7 @@ package pl.polprzewodnikowy.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.polprzewodnikowy.user.User;
 
 @Controller
 @RequestMapping("/")
@@ -10,6 +11,10 @@ class HomeController {
 
     @RequestMapping("")
     public String home(Model model) {
+        User user = new User();
+        user.setId(1);
+        user.setName("TestUser");
+        model.addAttribute("user", user);
         model.addAttribute("websiteTitle", "Test website");
         return "index";
     }
