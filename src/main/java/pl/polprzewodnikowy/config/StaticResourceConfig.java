4ed681e4fb5221df01a3eb4bc3df32512c525656
Polprzewodnikowy <polprzewodnikowy@gmail.com>
2018-07-31 17:40:56 +0200

    Changed directory structure

--- src/main/java/pl/polprzewodnikowy/config/StaticResourceConfig.java
+++ src/main/java/pl/polprzewodnikowy/config/StaticResourceConfig.java
@@ -5,11 +5,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StaticResourceConfig implements WebMvcConfigurer {
+class StaticResourceConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("file:resources/");
+        registry.addResourceHandler("/resources/images/**").addResourceLocations("file:resources/images/");
+        registry.addResourceHandler("/resources/css/**").addResourceLocations("file:resources/css/");
+        registry.addResourceHandler("/resources/js/**").addResourceLocations("file:resources/js/");
     }
 
 }
